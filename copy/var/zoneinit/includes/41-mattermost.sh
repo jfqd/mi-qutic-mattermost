2cf8eec3baf6379e2be596f9c6f0ec3f60dfd8b6
jfqd <jfqd@blun.org>
2019-09-18 12:28:59 +0200

    we may change these settings later

--- copy/var/zoneinit/includes/41-mattermost.sh
+++ copy/var/zoneinit/includes/41-mattermost.sh
@@ -1,2 +1,41 @@
 svccfg import /opt/local/lib/svc/manifest/mattermost.xml 
+
+# TODO: change these settings:
+# ServiceSettings.SiteURL
+# SqlSettings.DataSource
+# NotificationLogSettings.FileLocation
+# PasswordSettings.MinimumLength
+# FileSettings.Directory
+# EmailSettings.FeedbackEmail
+# EmailSettings.ReplyToAddress
+# EmailSettings.EnableSMTPAuth
+# EmailSettings.SMTPUsername
+# EmailSettings.SMTPPassword
+# EmailSettings.SMTPServer
+# EmailSettings.SMTPPort
+# EmailSettings.ConnectionSecurity
+# SupportSettings.TermsOfServiceLink
+# SupportSettings.PrivacyPolicyLink
+# SupportSettings.AboutLink
+# SupportSettings.HelpLink
+# SupportSettings.ReportAProblemLink
+# SupportSettings.SupportEmail
+# LocalizationSettings.DefaultServerLocale
+# LocalizationSettings.DefaultClientLocale
+# LocalizationSettings.AvailableLocales
+# NativeAppSettings.AppDownloadLink
+# NativeAppSettings.AndroidAppDownloadLink
+# NativeAppSettings.IosAppDownloadLink
+# ElasticsearchSettings.ConnectionUrl
+# ElasticsearchSettings.Username
+# ElasticsearchSettings.Password
+# ElasticsearchSettings.IndexPrefix
+# PluginSettings.Directory
+# GuestAccountsSettings.Enable
+
+# echo "* Create mattermost database"
+# CREATE DATABASE IF NOT EXISTS mattermost CHARACTER SET = 'utf8mb4' COLLATE = 'utf8mb4_unicode_ci';
+# CREATE USER 'mattermost'@'%' IDENTIFIED BY 'password';
+# GRANT ALL PRIVILEGES ON * . * TO 'mattermost'@'%';
+
 # svcadm enable -r svc:/application/mattermost:default
