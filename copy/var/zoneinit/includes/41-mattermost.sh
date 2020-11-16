6dbe8a01c6601ba32515b53e1196ab9d811e25dd
jfqd <jfqd@blun.org>
2020-11-16 21:42:29 +0100

    more work to do

--- copy/var/zoneinit/includes/41-mattermost.sh
+++ copy/var/zoneinit/includes/41-mattermost.sh
@@ -33,9 +33,16 @@ svccfg import /opt/local/lib/svc/manifest/mattermost.xml
 # PluginSettings.Directory
 # GuestAccountsSettings.Enable
 
+sed -i \
+    -e's#mmuser:mostest@tcp(localhost:3306)/mattermost_test#mattermost:mostest@tcp(localhost:3306)/mattermost#' \
+    /home/mattermost/mattermost/config/config.json
+
+# /usr/sbin/svcadm enable -r svc:/pkgsrc/percona:default
+
 # echo "* Create mattermost database"
 # CREATE DATABASE IF NOT EXISTS mattermost CHARACTER SET = 'utf8mb4' COLLATE = 'utf8mb4_unicode_ci';
-# CREATE USER 'mattermost'@'%' IDENTIFIED BY 'password';
-# GRANT ALL PRIVILEGES ON * . * TO 'mattermost'@'%';
+# CREATE USER 'mattermost_kxxxx'@'%' IDENTIFIED BY 'password';
+# https://github.com/mattermost/mattermost-server/issues/8432
+# GRANT ALTER, CREATE, DELETE, DROP, INDEX, INSERT, SELECT, UPDATE ON mattermost_kxxxx.* TO 'mattermost_kxxxx'@'%'
 
 # svcadm enable -r svc:/application/mattermost:default
